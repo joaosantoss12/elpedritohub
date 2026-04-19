66160d9a3f2261c91576ae963a6d58a6a55b465b
joaosantoss12 <souoduda2002@gmail.com>
2026-04-19 01:05:39 +0100

    chore: update create-checkout.ts

--- api/stripe/create-checkout.ts
+++ api/stripe/create-checkout.ts
@@ -26,7 +26,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
     const session = await stripe.checkout.sessions.create({
       mode: 'subscription',
-      payment_method_types: ['card', 'link', 'klarna'],
+      payment_method_types: ['card', 'link', 'klarna', 'mb_way'],
       line_items: [
         {
           quantity: 1,
